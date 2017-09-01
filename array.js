6584602526bba9cb9fff09e4e6dcee8e453c3a76
lichengcheng123 <3222811577@qq.com>
2017-09-01 09:52:31 +0800

    rm

---
+++ array.js
@@ -0,0 +1,15 @@
+//find 方法 ，（ 他的参数是一个回调函数 ）
+// let ob=[1, 4, -5, 10].find((array) => array < 0)
+// console.log(ob)
+
+
+//map 方法 ，（ 不会改变数组的长度 ）
+// let a =[1,2,3]
+// let b = a.map(t =>t*2)
+// console.log(b)
+
+
+//filter 方法 （ 筛选 ）
+let a =[3,4,21,11]
+let b =a .filter(t => t>10)
+console.log(b)
